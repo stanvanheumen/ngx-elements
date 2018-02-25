fadee43bea35bb6ae1266703213fabcc367c1bb1
Stan van Heumen <stan@bravobit.nl>
2018-02-25 16:29:24 +0100

    - Version 1.0.1; - Exposed all the elements in the public_api.ts.

--- library/public_api.ts
+++ library/public_api.ts
@@ -1,5 +1,15 @@
-export * from './src/forms/forms.module';
-
 export * from './src/cards/cards.module';
+export * from './src/cards/card/card.component';
+export * from './src/cards/modal/modal.component';
+
+export * from './src/forms/forms.module';
+export * from './src/forms/button/button.component';
+export * from './src/forms/checkbox/checkbox.component';
+export * from './src/forms/form-control/form-control.component';
+export * from './src/forms/group/group.component';
+export * from './src/forms/radio/radio.component';
 
 export * from './src/miscellaneous/miscellaneous.module';
+export * from './src/miscellaneous/directives/autoresize.directive';
+export * from './src/miscellaneous/directives/outside-click.directive';
+export * from './src/miscellaneous/loading/loading.component';
