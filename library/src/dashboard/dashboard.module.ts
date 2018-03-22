c84888a6a7160ac09ad782b25bad1570ce86d480
Stan van Heumen <stan@bravobit.nl>
2018-03-22 17:23:34 +0100

    - Router module fix.

--- library/src/dashboard/dashboard.module.ts
+++ library/src/dashboard/dashboard.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
 import {NgxStorageModule} from '@stanvanheumen/ngx-storage';
 
 import {NgxDashboardComponent} from './dashboard/dashboard.component';
 import {NgxNavigationComponent} from './navigation/navigation.component';
 
 @NgModule({
-    imports: [CommonModule, NgxStorageModule],
+    imports: [CommonModule, RouterModule, NgxStorageModule],
     declarations: [NgxDashboardComponent, NgxNavigationComponent],
     exports: [NgxDashboardComponent, NgxNavigationComponent]
 })
